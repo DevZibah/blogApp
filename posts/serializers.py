2d554a9b8642d611c390f7b9c651a801116c9cc4
Chisom Chukwuma <chukwumachisom18@gmail.com>
2022-01-05 20:52:22 -1100

    blogapp

--- posts/serializers.py
+++ posts/serializers.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 
 class PostSerializer(serializers.ModelSerializer):
 
-    author = serializers.SlugRelatedField(slug_field='username', required=True, queryset=User.objects.all())
+    username = serializers.SlugRelatedField(slug_field='username', required=True, queryset=User.objects.all())
     class Meta:
-        fields = ('id', 'author', 'title', 'body', 'created_at',)
-        model = Post
+        # fields = ('id', 'username', 'arts', 'science', 'IT', 'commercial', 'female', 'male', 'birthdate', 'christianity', 'muslim', 'other', 'AB', 'AD','AI','AN','BA','BY','BN','BR','CR','DL','EB','ED','EK','EN','GM','IM','JI','KA','KN','KS','KB','KG','KW','LA', 'NA','NI','OG','ON','OS','OY','PT','RV','ST','TB','YB','ZF','FC', 'loudchewing', 'beinglate', 'talkingduringamovie', 'talkingwhenyourmouthisfull', 'leavingthewaterrunning', 'smoking', 'leavingdirtydishesinthesink', 'sneezingwithoutcoveringyourmouth', 'littering', 'bitingnails', 'snoring', 'leavingthetoiletseatup')
+        fields = ('id', 'username', 'firstname', 'lastname')
+        model = Post 
